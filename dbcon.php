50cc7b6f4aa218cd806c0b0464dd9de0115cab3e
Avinash kumar singh <32649381+avinashsinghavin@users.noreply.github.com>
2020-07-11 21:37:23 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,6 @@
 <?php
 
-// $con=mysqli_connect('sql209.byethost.com','b10_26217097','.#Avin@sh1','b10_26217097_foodshala');
+// $con=mysqli_connect('sql209.byethost.com','b10_26217097','******','b10_26217097_foodshala');
 $con=mysqli_connect('localhost','root','','foodshala');
 // if(!$con){
 //     echo "DB Error";
